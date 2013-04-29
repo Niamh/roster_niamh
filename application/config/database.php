feadc6f204b48ba648905f9335d8ef85475f911c
Niamh Brady <niamhbrady719@gmail.com>
2013-04-29 15:10:39 +0100

    database files
    updated

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['hostname'] = 'tunnel.pagodabox.com:3306';
+$db['default']['username'] = 'lelia';
+$db['default']['password'] = '3yudGzHs';
+$db['default']['database'] = 'rostersystem';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
